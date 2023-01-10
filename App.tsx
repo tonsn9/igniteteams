d2e8724821c964f5d37541d1a7ee7980fa13d6d9
Ton <ton.sn9@gmail.com>
2023-01-10 11:02:08 -0300

    fix: omplementation for app

--- App.tsx
+++ App.tsx
@@ -1,13 +1,29 @@
-import { ThemeProvider } from 'styled-components';
+import { StatusBar } from "react-native";
 
-import theme from './src/theme';
+import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
-import { Groups } from '@screens/Groups';
+import { Loading } from "@components/Loading";
+
+import theme from "./src/theme";
+
+import { Groups } from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
